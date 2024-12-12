76a6378c547aee39ba9cc9ae5ff5a723c5213a50
user <user@JAYAPALKONDA>
2024-12-12 17:46:05 +0530

    task1 by developer jaipal

---
+++ Proj01UsingGitHub/src/main/java/com/nst/main/AirthmeticOperation.java
@@ -0,0 +1,17 @@
+package com.nst.main;
+
+//import java.util.Scanner;
+
+public class AirthmeticOperation {
+	
+	public int sum(int x , int y) {
+	
+		return x+y;
+	}
+public static void main (String[] args)
+{
+	AirthmeticOperation aop = new AirthmeticOperation();
+	System.out.println(aop.sum(10,20));
+	
+}
+}
--- Proj01UsingGitHub/src/test/java/com/nst/main/AppTest.java
+++ Proj01UsingGitHub/src/test/java/com/nst/main/AppTest.java
@@ -1,19 +1,76 @@
 package com.nst.main;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
+import com.nst.main.AirthmeticOperation;
 /**
  * Unit test for simple App.
  */
 public class AppTest {
 
-    /**
-     * Rigorous Test :-)
-     */
+	private  static AirthmeticOperation aop;
+   
+	@BeforeAll
+	public static void beforeTest()
+	{
+		//before code test
+		aop = new AirthmeticOperation();
+	}
+	
+	@BeforeEach
+	public  void beforeEach()
+	{
+		//before each
+	}
     @Test
-    public void shouldAnswerWithTrue() {
-        assertTrue(true);
+    public void shouldAnswerWithPositive() {
+    	
+    	//int a = 10 ;
+    	// b = 40 ;
+    	int expected = 50 ;
+    	int actual = aop.sum(30, 20);
+    	assertEquals(expected, actual);
     }
+    
+
+    @Test
+    public void shouldAnswerWithNegative() {
+    	
+    	//int a = -10 ;
+    	//int b = -40 ;
+    	int expected = -50 ;
+    	int actual = aop.sum(-10, -40);
+    	assertEquals(expected, actual);
+    }
+    
+
+    @Test
+    public void shouldAnswerWithMixed() {
+    	
+    	//int a = 10 ;
+    	//int b = -40 ;
+    	int expected = -30 ;
+    	int actual = aop.sum(10, -40);
+    	assertEquals(expected, actual);
+    }
+    
+    @AfterEach
+    public  void afterEach()
+    {
+    	//after each life cycle
+    }
+    
+    @AfterAll
+    public static void afterAll()
+    {
+    	//after all 
+    	aop = null ;
+    }
+    
 }
