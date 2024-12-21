dc8ff5755259847790251ed0b159977f11e148b0
user <user@JAYAPALKONDA>
2024-12-21 16:12:10 +0530

    TL-added some Extra features like withdraw and deposit

--- Proj01UsingGitHub/src/main/java/com/nst/Netbanking/NetbankingService.java
+++ Proj01UsingGitHub/src/main/java/com/nst/Netbanking/NetbankingService.java
@@ -1,5 +1,6 @@
 package com.nst.Netbanking;
 
+import java.util.Date;
 import java.util.Random;
 
 public class NetbankingService {
@@ -7,5 +8,16 @@ public class NetbankingService {
 	public String createAccount(String aadhar,double initialAmt, String type)
 	{
 		return "Account is opened "+ new Random().nextLong(500000000l);
+	}
+	
+	
+	public String withdrawAmount(String accno , Double Amount)
+	{
+		return Amount+" is debited from Account Number "+ accno + " on "+ new Date();
+	}
+	
+	public String depositeAmount(String accno , Double Amount)
+	{
+		return Amount+" is credited to Account Number "+ accno +" on "+ new Date();
 	}
 }
