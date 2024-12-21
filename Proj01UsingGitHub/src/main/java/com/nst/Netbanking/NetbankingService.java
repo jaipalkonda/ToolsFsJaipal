a11b307d01e5e62afe2b50720e0416df4d82df30
user <user@JAYAPALKONDA>
2024-12-21 16:06:50 +0530

    dev1-developed account opening logic

--- Proj01UsingGitHub/src/main/java/com/nst/Netbanking/NetbankingService.java
+++ Proj01UsingGitHub/src/main/java/com/nst/Netbanking/NetbankingService.java
@@ -1,5 +1,11 @@
 package com.nst.Netbanking;
 
+import java.util.Random;
+
 public class NetbankingService {
 
+	public String createAccount(String aadhar,double initialAmt, String type)
+	{
+		return "Account is opened "+ new Random().nextLong(500000000l);
+	}
 }
